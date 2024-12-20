d6c1718ee7d1ee64a8f820661fdc91706bfb4c6f
hartmandy <120344775+hartmandy@users.noreply.github.com>
2024-12-20 01:09:20 -0500

    dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,10 +54,20 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        scale: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
+        },
+        shadow: {
+          "0%, 100%": { opacity: "0" },
+          "50%": { opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        scale: "scale 1.5s infinite ease-in-out",
+        shadow: "shadow 1.5s infinite ease-in-out",
       },
     },
   },
